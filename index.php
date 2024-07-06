8bd42e0b2aeca3029a4b0fa0da94a6c829dd0595
birir1 <sospeterbirir1@gmail.com>
2024-07-06 11:17:14 +0300

    11:17

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo - repo</title>
+</head>
+
+<body>
+    <div class="containner">
+        <div clas s="row">
+            <div class="nav">
+                <ul>
+                    <li><a href="index.php">HOME</a></li>
+                    <li><a href="about.php">ABOUT</a></li>
+                    <li><a href="services.php">SERVICES</a></li>
+                    <li><a href="blog.php">BLOGS</a></li>
+                </ul>
+            </div>
+        </div>
+    </div> .
+</body>
+
+</html>
